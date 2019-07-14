fd4052e91671de7b9c88ccbf6c77df8573c502ae
Kondipati Satish <satish_kondipati@apple.com>
2019-07-14 13:32:38 -0700

    Changing the repo

--- ds-algos/src/com/sunrisers/dailycoding/problems/Problem_1.java
+++ ds-algos/src/com/sunrisers/dailycoding/problems/Problem_1.java
@@ -3,7 +3,7 @@ package com.sunrisers.dailycoding.problems;
 public class Problem_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub.
 
 	}
 
